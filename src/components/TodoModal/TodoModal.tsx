86c8f2f12a12b15d786d995bda2e155df14e602f
Nikita Doroshenko <nikitokd05@gmail.com>
2025-08-14 22:10:41 +0300

    task solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,43 +1,19 @@
 import React from 'react';
-import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { ModalCard } from '../ModalCard';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  modal: boolean;
+  selectedTodo: Todo | null;
+  onClose: () => void;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  modal,
+  selectedTodo,
+  onClose,
+}) => {
   return (
-    <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
-
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
-            </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-            </p>
-          </div>
-        </div>
-      )}
-    </div>
+    <>{modal === true && <ModalCard todo={selectedTodo} onClose={onClose} />}</>
   );
 };
